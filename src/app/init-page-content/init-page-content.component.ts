01ef574dedce99095dc1eaa51eef906f028689f0
Occ Coo <kylingourmand@gmail.com>
2017-08-04 08:05:07 +1000

    css to scss

--- src/app/init-page-content/init-page-content.component.ts
+++ src/app/init-page-content/init-page-content.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-init-page-content',
 	templateUrl: './init-page-content.component.html',
-	styleUrls: ['./init-page-content.component.css']
+	styleUrls: ['./init-page-content.component.scss']
 })
 export class InitPageContentComponent implements OnInit {
 
--- src/app/share-module/sidebar-outlet/sidebar-outlet.component.ts
+++ src/app/share-module/sidebar-outlet/sidebar-outlet.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
 	selector: 'app-sidebar-outlet',
 	templateUrl: './sidebar-outlet.component.html',
-	styleUrls: ['./sidebar-outlet.component.css']
+	styleUrls: ['./sidebar-outlet.component.scss']
 })
 export class SidebarOutletComponent implements OnInit {
 
